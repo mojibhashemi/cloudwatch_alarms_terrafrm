3bf93e53777819fec94cecac03baf5e872302693
EC2 Default User <ec2-user@ip-172-31-43-100.eu-west-1.compute.internal>
2022-11-08 18:57:31 +0000

    github action on terraform

---
+++ infra/alarmwatch.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "bank-sum-formetriks"
+  namespace                 = var.student_name
+  metric_name               = "bank_sum.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "0"
+  evaluation_periods        = "2"
+  period                    = "60"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of money in the bank exceeds 0 "
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = var.student_name
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = "mojib_hashemi@hotmail.com"
+}
